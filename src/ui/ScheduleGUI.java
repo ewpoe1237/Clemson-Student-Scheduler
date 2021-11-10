3dfa76f57a010c1d65ba4f03bf48daaf69499c0b
Jenna <jennahofseth@hotmail.com>
2021-11-10 11:40:36 -0500

    added student input prompt panel

--- src/ui/ScheduleGUI.java
+++ src/ui/ScheduleGUI.java
@@ -1,9 +1,13 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class ScheduleGUI {
     private JPanel wrapper;
@@ -11,7 +15,17 @@ public class ScheduleGUI {
     private JPanel rootCard;
     private JPanel SemesterOne;
     private JButton continueButtonOne;
-    private JPanel semOne;
+    private JPanel inputPrompts;
+    private JSlider semesterSlider;
+    private JSlider creditSlider;
+    private JButton continueButtonTwo;
+    private JPanel courseInputCard;
+    private JPanel courseInputs;
+    private JPanel inputCard;
+    private JRadioButton areYouInTheRadioButton;
+
+    private int semestersLeft = 10, maxCredits = 15;
+    private boolean honorsStudent = false;
 
     static ScheduleGUI myGUI = new ScheduleGUI();
 
@@ -23,7 +37,8 @@ public class ScheduleGUI {
         frame.setVisible(true);
 
         myGUI.wrapper.add(myGUI.rootCard, "rootCard");
-        myGUI.wrapper.add(myGUI.SemesterOne, "SemesterOne");
+        myGUI.wrapper.add(myGUI.inputCard, "inputCard");
+        myGUI.wrapper.add(myGUI.courseInputCard, "courseInputCard");
     }
 
     public ScheduleGUI() {
@@ -31,7 +46,23 @@ public class ScheduleGUI {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CardLayout cl = (CardLayout) wrapper.getLayout();
-                cl.show(wrapper, "SemesterOne");
+                cl.show(wrapper, "inputCard");
+            }
+        });
+        continueButtonTwo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                CardLayout cl = (CardLayout) wrapper.getLayout();
+                cl.show(wrapper, "courseInputCard");
+
+                semestersLeft = semesterSlider.getValue();
+                maxCredits = creditSlider.getValue();
+            }
+        });
+        areYouInTheRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                honorsStudent = !honorsStudent;
             }
         });
     }
